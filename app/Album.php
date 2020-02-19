92f5db8a2d6976bf54d37a7413b61102b2318f13
Nelson Fulfil <rockyxcoded@gmail.com>
2020-02-19 20:33:51 +0100

    Fix artiste name from showing in ablum title

--- app/Album.php
+++ app/Album.php
@@ -31,7 +31,7 @@ class Album extends Model
 
     public function getTitleAttribute($value)
     {
-        return ucwords($this->artiste. ' - ' .$value);
+        return ucwords($value);
     }
 
 
